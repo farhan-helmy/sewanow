4cfa4fa3cc26429d8815ea0cb2b0ecde4943d2a8
farhan-helmy <farhanhlmy@gmail.com>
2022-03-15 01:34:28 +0800

    change url

--- nuxt.config.js
+++ nuxt.config.js
@@ -43,7 +43,7 @@ export default {
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    baseURL: process.env.BASE_URL
+    baseURL: "http://dev.sewanow.com"
   },
 
   //auth module configuration
